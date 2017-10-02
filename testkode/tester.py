fb0a19f0fb31fedda40ca6bd6a9dce07711eef3a
Thomas Nyegaard-Signori <thomas.nyegaard.signori@gmail.com>
2017-10-02 15:30:41 +0200

    arabic data, test bed

--- testkode/tester.py
+++ testkode/tester.py
@@ -39,27 +39,29 @@ def featTransform(train_tweets, test_tweets):
     #print(test_features)
     return train_features, test_features, TfidfV.vocabulary
 
-def model_train(feats_train, labels):
+def model_train(train_features, train_labels):
     # s(f(x), g(x)) + loss function handled by this model
     model = NearestCentroid()
-    model.fit(feats_train, labels)
+    model.fit(train_features, train_labels)
     return model
 
-def predict(model, features_test):
+def predict(model, test_features):
     """Find the most compatible output class given the input `x` and parameter `theta`"""
-    preds = model.predict(features_test)
+    preds = model.predict(test_features)
     #preds_prob = model.predict_proba(features_test)  # probabilities instead of classes
     #print(preds)
     return preds
 
 if __name__ == '__main__':
+    pred = "../testkode/preds.txt"
+    
     if sys.argv[1] == '17':
         fp = "../testkode/data17/"
+        
         train = [fp + "train/anger-ratings-0to1.train.txt", fp + "train/fear-ratings-0to1.train.txt", fp + "train/joy-ratings-0to1.train.txt", fp + "train/sadness-ratings-0to1.train.txt"]
         test = [fp + "test/anger-ratings-0to1.test.target.txt", fp + "test/fear-ratings-0to1.test.target.txt", fp + "test/joy-ratings-0to1.test.target.txt", fp + "test/sadness-ratings-0to1.test.target.txt"]
         dev = [fp + "dev/anger-ratings-0to1.dev.txt", fp + "dev/fear-ratings-0to1.dev.txt", fp + "dev/joy-ratings-0to1.dev.txt", fp + "dev/sadness-ratings-0to1.dev.txt"]
-        pred = "../testkode/preds.txt"
-
+        
         train_features, train_labels, test_features, test_labels = extractFeatures(train, test) #ændr test til dev
 
         model = model_train(train_features, train_labels)
@@ -70,13 +72,30 @@ if __name__ == '__main__':
 
         printPredsToFile(test, pred, predictions) #ændr test til dev
         eval(pred)
-    else:
+    elif sys.argv[1] == '18':
         fp = "../testkode/data18/2018-EI-reg-En-train/"
         fp_test = "../testkode/data17/"
+
         train = [fp + "2018-EI-reg-En-anger-train.txt", fp + "2018-EI-reg-En-fear-train.txt", fp + "2018-EI-reg-En-joy-train.txt", fp + "2018-EI-reg-En-sadness-train.txt"]
         test = [fp_test + "test/anger-ratings-0to1.test.target.txt", fp_test + "test/fear-ratings-0to1.test.target.txt", fp_test + "test/joy-ratings-0to1.test.target.txt", fp_test + "test/sadness-ratings-0to1.test.target.txt"]
-        pred = "../testkode/preds.txt"
+        
+        train_features, train_labels, test_features, test_labels = extractFeatures(train, test) #ændr test til dev
 
+        model = model_train(train_features, train_labels)
+        predictions = predict(model, test_features)
+        
+        #print(predictions)
+        #print(train_labels)
+
+        printPredsToFile(test, pred, predictions) #ændr test til dev
+        eval(pred)
+    if sys.argv[1] == 'arabic':
+        fp = "../testkode/data18/2018-EI-reg-Ar-train/"
+        fp_dev = "../testkode/data18/2018-EI-reg-Ar-dev/"
+        
+        train = [fp + "2018-EI-reg-Ar-anger-train.txt", fp + "2018-EI-reg-Ar-fear-train.txt", fp + "2018-EI-reg-Ar-joy-train.txt", fp + "2018-EI-reg-Ar-sadness-train.txt"]
+        test = [fp_dev + "2018-EI-reg-Ar-anger-dev.txt", fp_dev + "2018-EI-reg-Ar-fear-dev.txt", fp_dev + "2018-EI-reg-Ar-joy-dev.txt", fp_dev + "2018-EI-reg-Ar-sadness-dev.txt"]
+        
         train_features, train_labels, test_features, test_labels = extractFeatures(train, test) #ændr test til dev
 
         model = model_train(train_features, train_labels)
