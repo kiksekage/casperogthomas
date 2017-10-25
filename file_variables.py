40d477757c65f41aaf3c201e370ffeca5c496cd4
Thomas Nyegaard-Signori <thomas.nyegaard.signori@gmail.com>
2017-10-25 15:18:27 +0200

    Big changes, added a lot of data and switches

---
+++ file_variables.py
@@ -0,0 +1,88 @@
+filename_dict = {'anger' : 0, 'fear' : 1, 'joy' : 2, 'sadness' : 3}
+
+pred = "../testkode/preds.txt"
+    
+fp = "../testkode/"
+fp17 = fp + "data17/"
+fp18 = fp + "data18/"
+
+preds = fp + "preds/"
+
+fp17_train = fp17 + "train/"
+train_class_17 = [fp17_train + "anger-ratings-0to1.train.txt", fp17_train + "fear-ratings-0to1.train.txt",
+            fp17_train + "joy-ratings-0to1.train.txt", fp17_train + "sadness-ratings-0to1.train.txt"]
+
+fp17_test = fp17 + "test/"
+test_class_17 = [fp17_test + "anger-ratings-0to1.test.target.txt", fp17_test + "fear-ratings-0to1.test.target.txt",
+           fp17_test + "joy-ratings-0to1.test.target.txt", fp17_test + "sadness-ratings-0to1.test.target.txt"]
+
+fp17_dev = fp17 + "dev/"
+dev_class_17 = [fp17_dev + "anger-ratings-0to1.dev.txt", fp17_dev + "fear-ratings-0to1.dev.txt",
+          fp17_dev + "joy-ratings-0to1.dev.txt", fp17_dev + "sadness-ratings-0to1.dev.txt"]
+
+########################### ENGLISH DATASETS, REG ###########################
+
+fp18_reg_train_en = fp18 + "reg/2018-EI-reg-En-train/"
+train_reg_18_en = [fp18_reg_train_en + "2018-EI-reg-En-anger-train.txt", fp18_reg_train_en + "2018-EI-reg-En-fear-train.txt",
+                 fp18_reg_train_en + "2018-EI-reg-En-joy-train.txt", fp18_reg_train_en + "2018-EI-reg-En-sadness-train.txt"]
+
+fp18_reg_dev_en = fp18 + "reg/2018-EI-reg-En-dev/"
+dev_reg_18_en = [fp18_reg_dev_en + "2018-EI-reg-En-anger-train.txt", fp18_reg_dev_en + "2018-EI-reg-En-fear-train.txt",
+                 fp18_reg_dev_en + "2018-EI-reg-En-joy-train.txt", fp18_reg_dev_en + "2018-EI-reg-En-sadness-train.txt"]
+
+########################### ARABIC DATASETS, REG ###########################
+
+fp18_reg_train_ar = fp18 + "reg/2018-EI-reg-Ar-train/"
+train_reg_18_ar = [fp18_reg_train_ar + "2018-EI-reg-Ar-anger-train.txt", fp18_reg_train_ar + "2018-EI-reg-Ar-fear-train.txt",
+                 fp18_reg_train_ar + "2018-EI-reg-Ar-joy-train.txt", fp18_reg_train_ar + "2018-EI-reg-Ar-sadness-train.txt"]
+
+fp18_reg_dev_ar = fp18 + "reg/2018-EI-reg-Ar-dev/"
+dev_reg_18_ar = [fp18_reg_dev_ar + "2018-EI-reg-Ar-anger-train.txt", fp18_reg_dev_ar + "2018-EI-reg-Ar-fear-train.txt",
+                 fp18_reg_dev_ar + "2018-EI-reg-Ar-joy-train.txt", fp18_reg_dev_ar + "2018-EI-reg-En-sadness-train.txt"]
+
+########################### SPANISH DATASETS, REG ###########################
+
+fp18_reg_train_es = fp18 + "reg/2018-EI-reg-Es-train/"
+train_reg_18_es = [fp18_reg_train_es + "2018-EI-reg-Ar-anger-train.txt", fp18_reg_train_es + "2018-EI-reg-Ar-fear-train.txt",
+                 fp18_reg_train_es + "2018-EI-reg-Ar-joy-train.txt", fp18_reg_train_es + "2018-EI-reg-Ar-sadness-train.txt"]
+
+fp18_reg_dev_es = fp18 + "reg/2018-EI-reg-Es-dev/"
+dev_reg_18_es = [fp18_reg_dev_es + "2018-EI-reg-Ar-anger-train.txt", fp18_reg_dev_es + "2018-EI-reg-Ar-fear-train.txt",
+                 fp18_reg_dev_es + "2018-EI-reg-Ar-joy-train.txt", fp18_reg_dev_es + "2018-EI-reg-En-sadness-train.txt"]
+
+########################### ENGLISH DATASETS, CLASS ###########################
+
+fp18_class_train_en = fp18 + "class/2018-EI-oc-En-train/"
+train_reg_18_en = [fp18_class_train_en + "2018-EI-oc-En-anger-train.txt", fp18_class_train_en + "2018-EI-oc-En-fear-train.txt",
+                 fp18_class_train_en + "2018-EI-oc-En-joy-train.txt", fp18_class_train_en + "2018-EI-oc-En-sadness-train.txt"]
+
+fp18_reg_dev_en = fp18 + "class/2018-EI-oc-En-dev/"
+dev_reg_18_en = [fp18_reg_dev_en + "2018-EI-oc-En-anger-train.txt", fp18_reg_dev_en + "2018-EI-oc-En-fear-train.txt",
+                 fp18_reg_dev_en + "2018-EI-oc-En-joy-train.txt", fp18_reg_dev_en + "2018-EI-oc-En-sadness-train.txt"]
+
+########################### ARABIC DATASETS, CLASS ###########################
+
+fp18_reg_train_ar = fp18 + "class/2018-EI-oc-Ar-train/"
+train_reg_18_ar = [fp18_reg_train_ar + "2018-EI-oc-Ar-anger-train.txt", fp18_reg_train_ar + "2018-EI-oc-Ar-fear-train.txt",
+                 fp18_reg_train_ar + "2018-EI-oc-Ar-joy-train.txt", fp18_reg_train_ar + "2018-EI-oc-Ar-sadness-train.txt"]
+
+fp18_reg_dev_ar = fp18 + "class/2018-EI-oc-Ar-dev/"
+dev_reg_18_ar = [fp18_reg_dev_ar + "2018-EI-oc-Ar-anger-train.txt", fp18_reg_dev_ar + "2018-EI-oc-Ar-fear-train.txt",
+                 fp18_reg_dev_ar + "2018-EI-oc-Ar-joy-train.txt", fp18_reg_dev_ar + "2018-EI-oc-En-sadness-train.txt"]
+
+########################### SPANISH DATASETS, CLASS ###########################
+
+fp18_reg_train_es = fp18 + "class/2018-EI-oc-Es-train/"
+train_reg_18_es = [fp18_reg_train_es + "2018-EI-oc-Ar-anger-train.txt", fp18_reg_train_es + "2018-EI-oc-Ar-fear-train.txt",
+                 fp18_reg_train_es + "2018-EI-oc-Ar-joy-train.txt", fp18_reg_train_es + "2018-EI-oc-Ar-sadness-train.txt"]
+
+fp18_reg_dev_es = fp18 + "class/2018-EI-oc-Es-dev/"
+dev_reg_18_es = [fp18_reg_dev_es + "2018-EI-oc-Ar-anger-train.txt", fp18_reg_dev_es + "2018-EI-oc-Ar-fear-train.txt",
+                 fp18_reg_dev_es + "2018-EI-oc-Ar-joy-train.txt", fp18_reg_dev_es + "2018-EI-oc-En-sadness-train.txt"]
+
+reg_file_list = [train_reg_18_en, dev_reg_18_en, train_reg_18_ar, dev_reg_18_ar, train_reg_18_es, dev_reg_18_es]
+class_file_list = [train_oc_18_en, dev_oc_18_en, train_oc_18_ar, dev_oc_18_ar, train_oc_18_es, dev_oc_18_es]
+
+#def filepath_returner(language, year, task):
+#    if task == 'reg':
+#        language+year       
---
+++ testkode/models.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append("../")
+
+from sklearn.neighbors import NearestCentroid
+from sklearn.linear_model import Perceptron
+from sklearn.ensemble import RandomForestRegressor
+
+#stop_words = [(x.strip()) for x in open('arabic-stop-words.txt','r').read().split('\n')]
+
+def train_perceptron(train_features, train_emotions):
+    perceptron = Perceptron()
+    perceptron.fit(train_features, train_emotions)
+    return perceptron
+
+def train_nearest_centroid(train_features, train_emotions):
+    nearest_centroid = NearestCentroid()
+    nearest_centroid.fit(train_features, train_emotions)
+    return nearest_centroid
+
+def train_random_forest(train_features, train_labels):
+    random_forest = RandomForestRegressor()
+    random_forest.fit(train_features, train_labels)
+    return random_forest
+
+def predictor(model, test_features):
+    preds = model.predict(test_features)
+    return preds
+
